dcbc756c2e2d849a8caf140a721927378a4a5428
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2025-02-03 20:01:23 +0800

    not sure why they get deleted

--- code/preparing-gloss-for-concepticon-mapping.R
+++
@@ -1,8 +0,0 @@
-# data preparation for testing the concepticon map_concepts function in terminal
-df <- readr::read_tsv('data/digitised-holle-list-in-stokhof-1980.tsv')
-df <- df[, "English"]
-df <- subset(df, !is.na(English))
-colnames(df) <- "GLOSS"
-df$NUMBER <- 1:nrow(df)
-filenames <- paste("Stokhof-1980-", nrow(df), ".tsv", sep = "")
-readr::write_tsv(df, file = paste("data/", filenames, sep = ""))
